b58f1191c26acf6487f049e7452eba4315dea6f1
Dmitry Simakov <VAGNER2909@yandex.ru>
2018-11-03 19:21:40 +0300

    Optimize SeedDatabaseWorker.

--- app/src/main/java/com/dmitrysimakov/kilogram/data/SeedDatabaseWorker.kt
+++ app/src/main/java/com/dmitrysimakov/kilogram/data/SeedDatabaseWorker.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.dmitrysimakov.kilogram.data.entity.*
 import com.dmitrysimakov.kilogram.di.workerInjection.AndroidWorkerInjection
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.google.gson.stream.JsonReader
 import javax.inject.Inject
 
@@ -16,35 +16,31 @@ class SeedDatabaseWorker(val context: Context, workerParams: WorkerParameters) :
 
     @Inject lateinit var database: KilogramDb
 
-    private var jsonReader: JsonReader? = null
-
     override fun doWork(): Result {
         AndroidWorkerInjection.inject(this)
 
         return try {
             with(database) {
-                equipmentDao().insert(listFromJson("equipment.json", Array<Equipment>::class.java))
-                exerciseTypeDao().insert(listFromJson("exercise_type.json", Array<ExerciseType>::class.java))
-                mechanicsTypeDao().insert(listFromJson("mechanics_type.json", Array<MechanicsType>::class.java))
-                muscleDao().insert(listFromJson("muscle.json", Array<Muscle>::class.java))
-                exerciseDao().insert(listFromJson("exercise.json", Array<Exercise>::class.java))
-                targetedMuscleDao().insert(listFromJson("targeted_muscle.json", Array<TargetedMuscle>::class.java))
+                equipmentDao().insert(listFromJson("equipment.json"))
+                exerciseTypeDao().insert(listFromJson("exercise_type.json"))
+                mechanicsTypeDao().insert(listFromJson("mechanics_type.json"))
+                muscleDao().insert(listFromJson("muscle.json"))
+                exerciseDao().insert(listFromJson("exercise.json"))
+                targetedMuscleDao().insert(listFromJson("targeted_muscle.json"))
 
-                measurementParamDao().insert(listFromJson("measurement_param.json", Array<MeasurementParam>::class.java))
+                measurementParamDao().insert(listFromJson("measurement_param.json"))
             }
             Result.SUCCESS
         } catch (e: Exception) {
             Log.e(tag, "Error seeding database", e)
             Result.FAILURE
-        } finally {
-            jsonReader?.close()
         }
     }
 
-    private fun <T> listFromJson(filename: String, clazz: Class<Array<T>>): List<T> {
+    private inline fun <reified T : List<*>> listFromJson(filename: String): T {
         val inputStream = context.assets.open(filename)
-        jsonReader = JsonReader(inputStream.reader())
-        val arr: Array<T> = Gson().fromJson(jsonReader, clazz)
-        return arr.toList()
+        JsonReader(inputStream.reader()).use {
+            return Gson().fromJson(it, object : TypeToken<T>() {}.type)
+        }
     }
 }
