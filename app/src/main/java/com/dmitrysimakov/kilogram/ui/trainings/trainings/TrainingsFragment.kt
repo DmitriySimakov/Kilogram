c4c57909ef831b14e78ad0e1ca79635378ba8b6b
Dmitry Simakov <VAGNER2909@yandex.ru>
2019-04-27 19:00:47 +0300

    Added scroll to chosen date in TrainingsFragment.

--- app/src/main/java/com/dmitrysimakov/kilogram/ui/trainings/trainings/TrainingsFragment.kt
+++ app/src/main/java/com/dmitrysimakov/kilogram/ui/trainings/trainings/TrainingsFragment.kt
@@ -1,15 +1,15 @@
 package com.dmitrysimakov.kilogram.ui.trainings.trainings
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dmitrysimakov.kilogram.R
 import com.dmitrysimakov.kilogram.util.AppExecutors
@@ -17,6 +17,7 @@ import com.dmitrysimakov.kilogram.util.getViewModel
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.fragment_trainings.*
+import java.util.*
 import javax.inject.Inject
 
 class TrainingsFragment : DaggerFragment() {
@@ -34,14 +35,15 @@ class TrainingsFragment : DaggerFragment() {
     }}
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_trainings, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
     
-        trainings_rv.adapter = adapter
-        trainings_rv.addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+        recyclerView.adapter = adapter
+        recyclerView.addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
         viewModel.trainingList.observe(this, Observer { adapter.submitList(it) })
 
         ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
@@ -51,7 +53,7 @@ class TrainingsFragment : DaggerFragment() {
                 viewModel.deleteTraining(training._id)
                 getViewModel(activity!!, viewModelFactory).onTrainingSessionFinished()
             }
-        }).attachToRecyclerView(trainings_rv)
+        }).attachToRecyclerView(recyclerView)
 
         activity?.fab?.show()
         activity?.fab?.setOnClickListener{
@@ -65,5 +67,33 @@ class TrainingsFragment : DaggerFragment() {
                 findNavController().navigate(TrainingsFragmentDirections.toCreateTrainingFragment())
             }
         }
+    }
+    
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.calendar, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+    
+    override fun onOptionsItemSelected(item: MenuItem?) = when (item?.itemId) {
+        R.id.calendar -> {
+            val calendar = Calendar.getInstance()
+            val curYear = calendar.get(Calendar.YEAR)
+            val curMonthOfYear = calendar.get(Calendar.MONTH)
+            val curDay = calendar.get(Calendar.DAY_OF_MONTH)
+            DatePickerDialog(context!!, DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, monthOfYear)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                (recyclerView.layoutManager as LinearLayoutManager).scrollToPositionWithOffset(
+                        viewModel.findPositionInTrainingList(calendar), 0)
+            }, curYear, curMonthOfYear, curDay).apply {
+                datePicker.minDate = 0
+                val millisecondsPerMonth = 30L * 24 * 60 * 60 * 1000
+                datePicker.maxDate = Date().time + millisecondsPerMonth
+                show()
+            }
+            true
+        }
+        else -> false
     }
 }
--- app/src/main/java/com/dmitrysimakov/kilogram/ui/trainings/trainings/TrainingsViewModel.kt
+++ app/src/main/java/com/dmitrysimakov/kilogram/ui/trainings/trainings/TrainingsViewModel.kt
@@ -3,12 +3,19 @@ package com.dmitrysimakov.kilogram.ui.trainings.trainings
 import androidx.lifecycle.ViewModel
 import com.dmitrysimakov.kilogram.data.entity.Training
 import com.dmitrysimakov.kilogram.data.repository.TrainingRepository
+import java.util.*
 import javax.inject.Inject
 
 class TrainingsViewModel @Inject constructor(private val repository: TrainingRepository) : ViewModel() {
 
     val trainingList = repository.loadTrainingRList()
 
+    fun findPositionInTrainingList(calendar: Calendar): Int {
+        val ms = calendar.time.time
+        val pos = trainingList.value?.indexOfFirst { it.start_time < ms }
+        return if (pos == null || pos == -1) 0 else pos
+    }
+    
     fun deleteTraining(id: Long) {
         repository.deleteTraining(id)
     }
