88eb4bf29d5851b6c030770424df86915b1eeded
Dmitry Simakov <VAGNER2909@gmail.com>
2020-01-12 23:16:01 +0300

    Returned old item_message layout

--- app/src/main/java/com/dmitrysimakov/kilogram/ui/subscriptions/messages/MessagesListAdapter.kt
+++ app/src/main/java/com/dmitrysimakov/kilogram/ui/subscriptions/messages/MessagesListAdapter.kt
@@ -1,10 +1,7 @@
 package com.dmitrysimakov.kilogram.ui.subscriptions.messages
 
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.FrameLayout.LayoutParams.WRAP_CONTENT
 import androidx.recyclerview.widget.DiffUtil
 import com.dmitrysimakov.kilogram.data.remote.Message
 import com.dmitrysimakov.kilogram.databinding.ItemMessageBinding
@@ -12,10 +9,7 @@ import com.dmitrysimakov.kilogram.ui.common.DataBoundListAdapter
 
 class MessagesListAdapter(
         private val vm: MessagesViewModel
-) : DataBoundListAdapter<Message, ItemMessageBinding>(
-        null,
-        MessageDiffCallback()
-) {
+) : DataBoundListAdapter<Message, ItemMessageBinding>(null, MessageDiffCallback()) {
     
     override fun createBinding(parent: ViewGroup): ItemMessageBinding = ItemMessageBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
@@ -23,17 +17,10 @@ class MessagesListAdapter(
     override fun bind(binding: ItemMessageBinding, item: Message) {
         binding.vm = vm
         binding.message = item
-        
-        val isUserMessage = item.senderId == vm.user.value?.id
-        binding.messageBox.layoutParams = FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
-            gravity = if (isUserMessage) Gravity.END else Gravity.START
-        }
     }
 }
 
 private class MessageDiffCallback : DiffUtil.ItemCallback<Message>() {
-    override fun areItemsTheSame(oldItem: Message, newItem: Message) =
-            oldItem.id == newItem.id
-    override fun areContentsTheSame(oldItem: Message, newItem: Message) =
-            oldItem == newItem
+    override fun areItemsTheSame(oldItem: Message, newItem: Message) = oldItem.id == newItem.id
+    override fun areContentsTheSame(oldItem: Message, newItem: Message) = oldItem == newItem
 }
