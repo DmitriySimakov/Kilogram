f60f2db6a7696a5584057c4b4931784f08aa932f
Dmitry Simakov <VAGNER2909@gmail.com>
2020-02-02 20:20:28 +0300

    Fixed bug with pagerAdapter in SubscriptionsTabFragment

--- app/src/main/java/com/dmitrysimakov/kilogram/ui/person_page/subscriptions_tab/SubscriptionsTabFragment.kt
+++ app/src/main/java/com/dmitrysimakov/kilogram/ui/person_page/subscriptions_tab/SubscriptionsTabFragment.kt
@@ -1,6 +1,7 @@
 package com.dmitrysimakov.kilogram.ui.person_page.subscriptions_tab
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,8 +28,8 @@ class SubscriptionsTabFragment : Fragment() {
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = getTabTitle(position)
         }.attach()
-    
-        viewPager.currentItem = args.pageId
+        
+        Handler().postDelayed({ viewPager.currentItem = args.pageId }, 100) // КОСТЫЛЬ
     }
     
     private fun getTabTitle(position: Int): String? {
