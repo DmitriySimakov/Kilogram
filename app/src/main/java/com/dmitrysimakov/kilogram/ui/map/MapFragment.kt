7b3843dc17530a64518be68a0b43a9d11d44ecaf
Dmitry Simakov <VAGNER2909@gmail.com>
2020-04-21 14:45:22 +0300

    Enable MyLocation button to MapFragment

--- app/src/main/java/com/dmitrysimakov/kilogram/ui/map/MapFragment.kt
+++ app/src/main/java/com/dmitrysimakov/kilogram/ui/map/MapFragment.kt
@@ -1,18 +1,22 @@
 package com.dmitrysimakov.kilogram.ui.map
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.dmitrysimakov.kilogram.R
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+
+private const val REQUEST_LOCATION_PERMISSION = 1
 
 class MapFragment : Fragment(), OnMapReadyCallback {
     
@@ -33,7 +37,32 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         map = googleMap
         
         val home = LatLng(59.986152, 30.330734)
-        map.addMarker(MarkerOptions().position(home))
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 15f))
+        
+        enableMyLocation()
+    }
+    
+    private fun enableMyLocation() {
+        val locationPermission = ContextCompat.checkSelfPermission(
+                requireContext(),
+                ACCESS_FINE_LOCATION
+        )
+        if (locationPermission  == PERMISSION_GRANTED) {
+            map.isMyLocationEnabled = true
+        } else {
+            ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(ACCESS_FINE_LOCATION),
+                    REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+    
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && (grantResults[0] == PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
+        }
     }
 }
