25f28dcb4163a8266fc2bd6761cb2ec5a0abae45
Dmitry Simakov <VAGNER2909@gmail.com>
2020-04-22 11:56:25 +0300

    Find device location

--- app/src/main/java/com/dmitrysimakov/kilogram/ui/map/MapFragment.kt
+++ app/src/main/java/com/dmitrysimakov/kilogram/ui/map/MapFragment.kt
@@ -6,39 +6,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.dmitrysimakov.kilogram.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 private const val REQUEST_LOCATION_PERMISSION = 1
+private const val DEFAULT_ZOOM = 15f
 
 class MapFragment : Fragment(), OnMapReadyCallback {
     
     private lateinit var map: GoogleMap
     
+    private lateinit var locationClient: FusedLocationProviderClient
+    
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
     
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        locationClient = LocationServices.getFusedLocationProviderClient(requireContext())
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
     
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        
-        val home = LatLng(59.986152, 30.330734)
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 15f))
-        
         enableMyLocation()
     }
     
@@ -49,12 +52,9 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         )
         if (locationPermission  == PERMISSION_GRANTED) {
             map.isMyLocationEnabled = true
+            moveToMyLocation()
         } else {
-            ActivityCompat.requestPermissions(
-                    requireActivity(),
-                    arrayOf(ACCESS_FINE_LOCATION),
-                    REQUEST_LOCATION_PERMISSION
-            )
+            requestPermissions(arrayOf(ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
         }
     }
     
@@ -63,6 +63,14 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             if (grantResults.isNotEmpty() && (grantResults[0] == PERMISSION_GRANTED)) {
                 enableMyLocation()
             }
+        }
+    }
+    
+    private fun moveToMyLocation() = lifecycleScope.launch {
+        val location = locationClient.lastLocation.await()
+        if (location != null) {
+            val latLng = LatLng(location.latitude, location.longitude)
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM))
         }
     }
 }
